25fab2e0f5ad48318f61930698d627cac3d36e06
qosole <williamj99wei@gmail.com>
2022-08-18 18:23:22 -0500

    Add router for thought routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction
+} = require('../../controllers/thoughtController');
+
+// /api/thoughts
+router.route('/')
+    .get(getThoughts)
+    .post(createThought);
+
+// /api/thoughts/:thoughtId
+router.route('/:thoughtId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions')
+    .post(createReaction)
+
+router.route('/:thoughtId/reactions/:reactionId')
+    .delete(deleteReaction)
+
+module.exports = router;
