cc26adacc8312b25ce622ee9cb3249e7424ad650
qosole <williamj99wei@gmail.com>
2022-08-18 17:56:02 -0500

    Add routes for adding and removing a friend from a user's friends list

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -4,7 +4,9 @@ const {
   getSingleUser,
   createUser,
   updateUser,
-  deleteUser
+  deleteUser,
+  addFriend,
+  deleteFriend
 } = require('../../controllers/userController');
 
 // /api/users
@@ -17,5 +19,10 @@ router.route('/:userId')
     .get(getSingleUser)
     .put(updateUser)
     .delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId')
+    .post(addFriend)
+    .delete(deleteFriend)
 
 module.exports = router;
