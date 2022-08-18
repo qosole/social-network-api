647fc07af8e2421a8f122c49e0d2af9c968b73ee
qosole <williamj99wei@gmail.com>
2022-08-18 17:52:08 -0500

    Add controller to add friend to friends list

--- controllers/userController.js
+++ controllers/userController.js
@@ -49,4 +49,18 @@ module.exports = {
       .then(() => res.json({ message: 'User and associated thoughts deleted!' }))
       .catch((err) => res.status(500).json(err));
   },
+  // Add a friend by ID to a user's friends list by ID
+  addFriend(req, res) {
+    User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $addToSet: { friends: req.params.friendId } },
+        { runValidators: true, new: true }
+    )
+        .then((user) => 
+            !user
+                ? res.status(404).json({ message: 'No user with this id! '})
+                : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+  },
 };
