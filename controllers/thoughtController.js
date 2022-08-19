8bb412cb878d8b17015b7b50f1d61c1ddad0b11b
qosole <williamj99wei@gmail.com>
2022-08-18 19:41:54 -0500

    Fix bug with reactions not deleting

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -84,7 +84,7 @@ module.exports = {
   deleteReaction(req, res) {
     Thought.findOneAndUpdate(
       { _id: req.params.thoughtId },
-      { $pull: { reactions: req.params.reactionId } },
+      { $pull: { reactions: { reactionId: req.params.reactionId } } },
       { runValidators: true, new: true }
     )
       .then((thought) =>
